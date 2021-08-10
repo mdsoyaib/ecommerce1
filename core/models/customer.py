e76f070ccd51dbba0417e13a00d5756d42892e98
Md Soyaib <mdsoyaib2025@gmail.com>
2021-08-11 03:20:09 +0600

    working for login

--- core/models/customer.py
+++ core/models/customer.py
@@ -10,6 +10,14 @@ class Customer(models.Model):
     def register(self):
         self.save()
 
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email = email)
+        except:
+            return False
+
+
     def isExits(self):
         if Customer.objects.filter(email = self.email):
             return True
--- core/views.py
+++ core/views.py
@@ -96,4 +96,16 @@ def login(request):
     if request.method == "GET":
         return render(request, "login.html")
     else:
-        return HttpResponse("logged in")
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        customer = Customer.get_customer_by_email(email)
+        error_message = None
+        if customer:
+            flag = check_password(password, customer.password) 
+            if flag:
+                return redirect('homepage')
+            else:
+                error_message = "Email or Password Invalid!!"
+        else:
+            error_message = "Email or Password Invalid!!"
+        return render(request, 'login.html', {'error': error_message})
